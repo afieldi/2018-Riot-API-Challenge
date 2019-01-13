557f3c88455ccc7313315f0173a4c44e68f607ac
Vexrax <joshuaf_111@live.com>
2019-01-13 16:45:33 -0500

    adding endpoints for creating lobby, invite players, accepting invite.

---
+++ Client/src/renderer/league/LCUHelper.ts
@@ -0,0 +1,21 @@
+import LeagueConnection from "./league";
+
+export class LCUHelper
+{
+    constructor(private leagueconnection: LeagueConnection){}
+
+    public CreateCustomLobby()
+    {
+        //this.leagueconnection.request("", "POST", "")
+    }
+
+    public InviteToLobby(players: any)
+    {
+        //todo
+    }
+
+    public AcceptLobbyInvite(player: any)
+    {
+        //todo
+    }
+}
---
+++ Client/src/renderer/league/ServerRoutes.ts
@@ -0,0 +1,18 @@
+import LeagueConnection from "./league";
+import {Express} from "express";
+import {ResponseRequest} from "request";
+import {LCUHelper} from "./LCUHelper";
+
+export function setupRoutes(app: Express, leagueconnection: LeagueConnection)
+{
+    const lcuhelper = new LCUHelper(leagueconnection);
+    app.route('/lol/create-lobby').get((req: ResponseRequest, res:any) => {
+        lcuhelper.CreateCustomLobby();
+    });
+    app.route('/lol/invite/').get((req: ResponseRequest, res:any) => {
+        lcuhelper.InviteToLobby(req.body);
+    });
+    app.route('/lol/accept-lobby').get((req: ResponseRequest, res:any) => {
+        lcuhelper.AcceptLobbyInvite(req.body);
+    });
+}
