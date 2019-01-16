3d111c6b6f2fde12922f5a10251084b95330195d
Earleking <arek.fielding@gmail.com>
2019-01-16 11:53:00 -0500

    started work on manager

--- Manager/index.ts
+++ Manager/index.ts
@@ -1,1 +1,13 @@
 // This will actually manage the backend stuff like assigning missions and creating clan wars
+
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+
+var app = express();
+// Configure app
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.route("/")
+
+app.listen(8000);
