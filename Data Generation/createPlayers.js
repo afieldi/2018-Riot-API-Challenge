68e8a28cac3da632f2b66eb3e8add3cb00e0fffa
Earleking <arek.fielding@gmail.com>
2019-01-19 11:34:08 -0500

    added autogeneration of players and clans

---
+++ Data Generation/createPlayers.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+var request = require('request');
+var clans = [];
+
+for(var i = 1; i <= 50; i ++) {
+    clans.push({
+        "tag": "CLN" + i,
+        "name": "Clan " + i
+    });
+}
+
+fs.readFile("words.txt", 'utf8', (err, data) => {
+    data = data.split('\n');
+    var max = data.length;
+    for(var i = 0; i < 1000; i ++) {
+        var clan = rand(clans.length);
+        var options = {
+            form: {
+                "display_name": data[rand(max)] + " " + data[rand(max)],
+                "puuid": makepuuid(),
+                "id": makeid(),
+                "accountId": makeid(),
+                "clan_name": clans[clan]["name"],
+                "clan_tag": clans[clan]["tag"]
+            }
+        }
+        console.log(options);
+        request.put("http://localhost:1000/player", options, (err, res, data) => {
+            console.log(data);
+        });
+    }
+});
+
+function rand(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function makepuuid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+    for (var i = 0; i < 50; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+}
+
+function makeid() {
+    var text = "";
+    var possible = "123456789";
+  
+    for (var i = 0; i < 6; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  
+    return text;
+}
