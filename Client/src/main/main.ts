b9a25782b55c85b9b406c5228fe33d4192dfdb7c
Earleking <arek.fielding@gmail.com>
2019-01-23 17:47:18 -0500

    now using ngrok

--- Client/src/main/main.ts
+++ Client/src/main/main.ts
@@ -2,7 +2,7 @@ import {RunProxy} from "../renderer";
 import {RunServerWatcher} from "../renderer/ServerIndex";
 
 const localtunnel = require('localtunnel');
-
+const ngrok = require('ngrok');
 //const url = require("url");
 const path = require("path");
 const PORT = 49000 + (100 * Math.random())|0;
@@ -17,16 +17,19 @@ const { app, Tray } = require('electron');
 const iconPath = path.join(__dirname, 'MainIcon.png');
 
 let tray = null;
-app.on('ready', () => {
+app.on('ready', async () => {
     tray = new Tray(iconPath);
     tray.setToolTip('Make Clubs Great Again');
-    var mytunnel = localtunnel(SERVER_PORT, (err:any, tunnel:any) => {
-        if(err) throw err;
-        console.log("TUNNEL CREATED!!!!");
-        console.log(tunnel.url);
-        RunProxy(PORT, REPLACE_PORT, PWD, tunnel.url);
-        RunServerWatcher(PORT, PWD, SERVER_PORT);
-    });
+    const url = await ngrok.connect(4800);
+    console.log("TUNNEL CREATED!!!!");
+    RunProxy(PORT, REPLACE_PORT, PWD, url);
+    RunServerWatcher(PORT, PWD, SERVER_PORT);
+    // var mytunnel = localtunnel(SERVER_PORT, {"subdomin": "loldomain"} ,(err:any, tunnel:any) => {
+    //     if(err) throw err;
+    //     console.log("TUNNEL CREATED!!!!");
+    //     RunProxy(PORT, REPLACE_PORT, PWD, tunnel.url);
+    //     RunServerWatcher(PORT, PWD, SERVER_PORT);
+    // });
     
 });
 
